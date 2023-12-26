736618643a8494b8ab1e626b7800ee831f40c211
Md-Asikur <asikurrahaman997@gamil.com>
2023-12-26 20:15:02 +0600

    x

--- utils/base_url.ts
+++ utils/base_url.ts
@@ -1,1 +1,4 @@
-export const BASE_URL = "https://next-server-query-data-fetching.vercel.app/api";
+export const BASE_URL =
+  process.env.NODE_ENV === "production"
+    ? "https://next-server-query-data-fetching.vercel.app/api"
+    : "http://localhost:3000/api"; // Change the port if needed
