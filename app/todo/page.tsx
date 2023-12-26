d28fc1acbc82f822aa45c942900b7b5feab020ae
Md-Asikur <asikurrahaman997@gamil.com>
2023-12-26 19:59:10 +0600

    s

--- app/todo/page.tsx
+++ app/todo/page.tsx
@@ -2,17 +2,20 @@ import TodoComponent from '@/components/todo/Todo';
 import FetchTodo from "@/app/todo/fetchTodo";
 
 import React from 'react'
+import HomeDataFetch from '../dataFetch/page';
 
-const page = () => {
+const page = ({ searchParams }:any) => {
   return (
     <div>
       {/* <TodoComponent />
        */}
-      
-      <TodoComponent />
-      <FetchTodo/>
+
+      {/* <TodoComponent />
+      <FetchTodo/> */}
+
+      <HomeDataFetch searchParams={searchParams} />
     </div>
   );
-}
+};
 
 export default page
